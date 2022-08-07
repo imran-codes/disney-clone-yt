74c30f1b82eca78e9aab1dbdb0f5c98ad188f3ed
imranh481 <imranh160@gmail.com>
2022-08-07 09:33:25 +0100

    Added HTML render in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,41 @@
-import Document, { DocumentContext } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, {
+  DocumentContext,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import Head from "next/head";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: [initialProps.styles, sheet.getStyleElement()],
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
